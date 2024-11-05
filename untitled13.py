49be83523750472bd8c1fdb692625daa07556ade
MaxIA-l <maxi2004lda@gmail.com>
2024-11-05 15:46:40 -0300

    Update untitled13.py

--- untitled13.py
+++ untitled13.py
@@ -7,3 +7,9 @@ Original file is located at
     https://colab.research.google.com/drive/1IygClG5BMoNZ1kcEG-9fuMENEDYmd099
 """
 
+st.sidebar.title("Pagina del fornai og ")
+st.sidebar.header("Hola Gamer")
+st.sidebar.write("Espero que te encuentres bien")
+
+
+st.sidebar.image()
