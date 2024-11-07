7d67aab7da9425cb5f4edc737c0fd64db1066d27
MaxIA-l <maxi2004lda@gmail.com>
2024-11-06 22:43:59 -0300

    Update untitled13.py

--- untitled13.py
+++ untitled13.py
@@ -11,19 +11,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-st.sidebar.title("Pagina del fornai og ")
-st.sidebar.header("Hola Gamer")
+st.sidebar.title("Pagina del apoyo para el juego Fortnite ")
+st.sidebar.header("Saludos Gamer")
 st.sidebar.write("Espero que te encuentres bien")
 
 st.sidebar.image("OIP.jpg")
 
 
 if st.sidebar.button("Haz click abajo"):
-   st.sidebar.write("Haz hecho click en el boton")
+   st.sidebar.write("Más abajo")
 
                      
 user_input= st.sidebar.text_input("Escribe tu usuario:")
-st.sidebar.write("Hola", user_input)
+st.sidebar.write("Hola", user_input"En esta pagina te daremos una pequeña ayuda para mejorar en este Videojuego")
 
 st.title("Mejora tu calidad de juego")
 st.write("Para empezar a mejorar en este juego, necesitaremos una buena configuracion de construccion y edicion")
@@ -31,3 +31,9 @@ st.write("Aqui abajo te dejare un link para que conozca una de las mejores confi
 st.write("Recuerda que estos videos son de apoyo y que la mejor configuracion siempre sera la que a ti te acomode")
 link_video= "https://youtu.be/LMXX54rQ3Eo?si=Ag8yEGfE9KvftuwI"
 st.video(link_video)
+
+st.write("Ahora que sabemos que configuracion nos acomoda más, debemos conocer cuales armas tenemos esta temporada y conocerlas")
+st.write("Abajo te dejare un link con el cual podras ver cuales son las nuevas armas, como se utilizan y que tan buenas son")
+url_video="https://youtu.be/TLrOiFsoQe8?si=BUr7ECIIJy0XI6ek"
+st.video(url_video)
+
