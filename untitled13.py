e644c66279ac2a708cf91193b53a4323c06865fe
MaxIA-l <maxi2004lda@gmail.com>
2024-11-06 22:25:33 -0300

    Update untitled13.py

--- untitled13.py
+++ untitled13.py
@@ -27,3 +27,7 @@ st.sidebar.write("Hola", user_input)
 
 st.title("Mejora tu calidad de juego")
 st.write("Para empezar a mejorar en este juego, necesitaremos una buena configuracion de construccion y edicion")
+st.write("Aqui abajo te dejare un link para que conozca una de las mejores configuraciones de este juego")
+st.write("Recuerda que estos videos son de apoyo y que la mejor configuracion siempre sera la que a ti te acomode")
+link_video= "https://youtu.be/LMXX54rQ3Eo?si=Ag8yEGfE9KvftuwI"
+st.video(link_video)
